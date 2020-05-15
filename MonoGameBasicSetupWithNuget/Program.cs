adaf095756c4d91922d1b870261ec3a6896850ee
Daniel Jawna <djawna@gmail.com>
2020-05-15 23:42:40 +0200

    first commit

---
+++ MonoGameBasicSetupWithNuget/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGameBasicSetupWithNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new TheMonoGameGodClass()) {
+                game.Run();
+            }
+        }
+    }
+}
